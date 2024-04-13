84ff4c0565e0bdc728c5c74482675069c7e1880f
James Butcher <jamesbutcher@duck.com>
2024-04-13 18:28:31 +0100

    Add Lambdas and scoped environments

--- app/Main.hs
+++ app/Main.hs
@@ -10,14 +10,16 @@ import System.IO
 import qualified Data.HashMap.Strict as Map (HashMap, empty, insert, lookup)
 
 type Environment = Map.HashMap String Token
+type EnvStack = [Environment]
 
-data Token = Ident String | Expr [Token] | Num Float | Nil | Str String | Pair Token Token | Boolean Bool
+data Token = Ident String | Expr [Token] | Num Float | Nil | Str String | Pair Token Token | Lambda [String] [Token] | Boolean Bool
 
 instance Show Token where
   show (Num x) = show x
   show (Ident x) = x
   show (Expr [Ident "quote", x]) = '\'' : show x
   show (Expr (x:xs)) = '(' : foldl (\acc a -> acc ++ " " ++ show a) (show x) xs ++ ")"
+  show (Lambda _ _) = "#lambda"
   show (Expr []) = "nil"
   show (Pair x y) = '(':show x ++ " . " ++ show y ++ ")"
   show Nil = "nil"
@@ -69,30 +71,66 @@ nil = string "nil" >> pure Nil
 values :: Parsec String st [Token]
 values = many value 
 
-evalArgs :: Environment -> [Token] -> IO (Either String [Token])
+searchEnv :: EnvStack -> String -> Maybe Token
+searchEnv [] _ = Nothing
+searchEnv (x:xs) ident =
+  case Map.lookup ident x of
+    Nothing -> searchEnv xs ident
+    x -> x
+
+evalArgs :: EnvStack -> [Token] -> IO (Either String [Token])
 evalArgs env xs = sequence <$> mapM (evalNoEnv env) xs
 
-evalNoEnv :: Environment -> Token -> IO (Either String Token)
+evalNoEnv :: EnvStack -> Token -> IO (Either String Token)
 evalNoEnv env x = (fmap . fmap) fst (eval env x)
 
-eval :: Environment -> Token -> IO (Either String (Token, Environment))
+evalLambda :: EnvStack -> [String] -> [Token] -> [Token] -> IO (Either String Token)
+evalLambda env formals body args =
+  if length formals == length args
+     then evalBody new_env body (return $ Right Nil)
+     else return $ Left $ "(not (eq? (length formals) (length args)))" ++ " " ++ show formals ++ " " ++ show args
+  where
+    new_env = bindFormals formals args Map.empty : env
+    bindFormals :: [String] -> [Token] -> Environment -> Environment
+    bindFormals [] [] acc = acc
+    bindFormals (formal:xs) (arg:ys) acc = Map.insert formal arg (bindFormals xs ys acc)
+    evalBody :: EnvStack -> [Token] -> IO (Either String Token) -> IO (Either String Token)
+    evalBody envs [] acc = return $ Right Nil
+    evalBody envs (x:xs) acc = do
+      res <- eval envs x
+      case res of
+        Right (token, envs') -> evalBody envs' xs (pure $ Right token)
+        Left err -> return $ Left err
+    
 
-eval env (Expr ((Ident "define"):xs)) =
+eval :: EnvStack -> Token -> IO (Either String (Token, EnvStack))
+
+eval env (Expr ((Ident "lambda"):xs)) =
+  return $ case xs of
+    (Expr formals:body) -> case reduceFormals formals [] of
+      Just x -> Right (Lambda x body, env)
+      _ -> Left "Expect (lambda (ident ...) _ ...)"
+    _ -> Left "Expected (lambda sexp _ ...)"
+    where
+      reduceFormals :: [Token] -> [String] -> Maybe [String]
+      reduceFormals [] acc = Just acc
+      reduceFormals (Ident x : xs) acc = reduceFormals xs $ acc ++ [x]
+      reduceFormals _ _ = Nothing
+
+eval env (Expr ((Lambda formals args):xs)) =
+  fmap (\x -> (x, env)) <$> evalLambda env formals args xs
+
+eval env@(current:rest) (Expr ((Ident "define"):xs)) =
   case xs of
-    [Ident label, Ident ident] -> do
-      res <- evalNoEnv env (Ident ident)
-      return $ case res of
-        Right sexp -> Right (Nil, Map.insert label sexp env)
-        Left err -> Left err
     [Ident label, x] -> do
       res <- evalNoEnv env x
       return $ do
         sexp <- res 
-        Right (Nil, Map.insert label sexp env)
+        Right (Nil, Map.insert label sexp current : rest)
     _ -> return $ Left "Expected (define ident _)"
 
 eval env (Ident x) =
-  return $ case Map.lookup x env of
+  return $ case searchEnv env x of
     Just x -> Right (x, env)
     _ -> Left "Unbound variable"
 
@@ -169,11 +207,16 @@ eval env (Expr ((Ident "input"):xs)) =
     [] -> (\x -> Right (Str x, env)) <$> getLine
     _ -> return $ Left "Expected (input)"
 
+eval env (Expr ((Ident ident):xs)) =
+  case searchEnv env ident of
+    Just (Lambda formals args) -> fmap (\x -> (x, env)) <$> evalLambda env formals args xs
+    _ -> return $ Left "Blahhhh"
+
 eval env (Expr _) = return $ Left "Ill-formed expression"
 
 eval env x = return $ Right (x, env)
 
-repl :: Environment -> IO ()
+repl :: EnvStack -> IO ()
 repl env = do
   putStr "> "
   hFlush stdout
@@ -187,4 +230,4 @@ repl env = do
         Left err -> print err >> repl env
 
 main :: IO ()
-main = repl Map.empty
+main = repl [Map.empty]

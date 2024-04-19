7afa28e51312e48bd4b7e7942134c4dad28f8b22
James Butcher <jamesbutcher@duck.com>
2024-04-20 00:58:25 +0100

    Add evalFile

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
-import Parse (value)
+import Parse (Token (..), value, values)
 import Eval (EnvStack, eval)
 
 import Data.HashMap.Strict (empty) 
 import Text.Parsec (parse)
 import System.IO
+import System.Environment (getArgs)
 
 repl :: EnvStack -> IO ()
 repl env = do
@@ -20,5 +21,26 @@ repl env = do
         Right (x, new_env) -> print x >> repl new_env
         Left err -> print err >> repl env
 
+evalLines :: String -> IO () 
+evalLines content = do
+  case parse values "" content of
+    Left err -> print err
+    Right xs -> evalValues xs $ pure . Right $ (Nil, [empty])
+  where
+    evalValues :: [Token] -> IO (Either String (Token, EnvStack)) -> IO ()
+    evalValues [] acc = acc >> pure () 
+    evalValues (x:xs) acc = do
+      acc' <- acc
+      case acc' of
+        Left err -> putStr err
+        Right (_, env) -> evalValues xs $ eval env x
+
 main :: IO ()
-main = repl [empty]
+main = do
+  args <- getArgs
+  case args of
+    [path] -> do
+      contents <- readFile path
+      evalLines contents
+    [] -> repl [empty]
+    _ -> putStr "Could not resolve query"

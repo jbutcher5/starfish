7b1238efd67c8f3b2ebb99252e33754187ce0cb8
James Butcher <jamesbutcher167@gmail.com>
2025-07-13 16:06:52 +0100

    Add FrameFunc and LeafFunc

--- app/Compile.hs
+++ app/Compile.hs
@@ -2,37 +2,63 @@ module Compile where
 
 import Parse (Token (..))
 import GHC.Float (float2Int)
+import Data.Bits ((.&.))  
 
-data IR = Func String [IR] | Variable String Int Int | Call String | Inline String
+data IR = FrameFunc String Word [IR] |
+          LeafFunc String [IR] |
+          Variable String Word Word Int |
+          Call String [IR] |
+          Inline String | Immediate Int
 
 token2ir :: Token -> Maybe IR
 token2ir (Expr ((Ident "define"):xs)) =
   case xs of
-    [Ident label, Num x] -> Just $ Variable label 4 $ float2Int x 
+    [Ident label, Num x] -> Just . Variable label 4 0 $ float2Int x 
     _ -> Nothing
 token2ir (Expr ((Ident "defun"):(Ident name):xs)) = do
   ys <- mapM token2ir xs
-  Just $ Func name ys
+  let (ir, space) = foldr (\x (acc, rbp) -> case x of
+                  Variable ident bytes _ dat ->
+                    (Variable ident bytes (bytes + rbp) dat:acc, rbp + bytes)
+                  _ -> (x:acc, rbp)) ([], 0) ys :: ([IR], Word)
+  Just $ FrameFunc name ((0xFFFFFFFFFFFFFFF0 .&. space) + 16) ir 
 token2ir (Expr [Ident "asm", Str asm]) = Just $ Inline asm
+token2ir (Num x) = Just . Immediate $ float2Int x 
+token2ir (Expr ((Ident fname):xs)) = do
+  ys <- mapM token2ir xs
+  Just $ Call fname ys
 token2ir _ = Nothing
 
-instr2asm :: IR -> Int -> (String, Int)
-instr2asm (Variable ident b x) rbp
-  = ("mov DWORD [rbp-" <> show (rbp + b) <> "], " <> show x, b)
-instr2asm (Func name body) rbp
-  = (name <> ":\npush rbp\nmov rbp, rsp" <> ir2asm body <> "\npop rbp\nret", 0)
-instr2asm (Inline asm) _ = (asm, 0)
-instr2asm _ _ = ("", 0)
+instr2asm :: IR -> String
+instr2asm (Immediate x) = "\nmov rax, " <> show x
+instr2asm (Variable ident bytes rbp x)
+  = "\nmov DWORD [rbp-" <> show rbp <> "], " <> show x
+instr2asm (FrameFunc name space body) 
+  = "\n" <> name <> ":\npush rbp\nmov rbp, rsp\nsub rsp, " <> show space <> ir2asm body <> "\nleave\nret"
+instr2asm (Inline asm) = "\n" <> asm
+instr2asm (Call fname args) = "\ncall " <> fname
 
-accumulate :: IR -> (String, Int) -> (String, Int)
-accumulate ir (program, rbp) = ("\n" <> asm <> "\n" <> program, rbp + offset)
-  where (asm, offset) = instr2asm ir rbp 
+-- TODO: Implement function calls 
+-- instr2asm (Call fname args) _ = (buildParams args <> "call " <> fname, 0)
+--   where
+--     paramRegister = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"]
+--     buildParams :: [IR] -> String
+--     buildParams = setupParams . zip . paramRegister 
+
+--     setupParams :: [(String, IR)] -> String
+--     setupParams x = setupParams' x ""
+    
+--     setupParams' :: [(String, IR)] -> String -> String
+--     setupParams' [] acc = acc
+--     setupParams' ((reg, ir):xs) acc = setupParams xs
+--      $ acc <> (fst $ instr2asm ir) <> "\nmov " <> reg <> " rax"
+
+instr2asm _ = ""
 
 ir2asm :: [IR] -> String
-ir2asm ir = program
-  where (program, _) = foldr accumulate ("", 0) ir
+ir2asm = foldr (\x acc -> instr2asm x <> acc) ""
 
 compile :: [Token] -> Maybe String
 compile tokens = do
   ir <- mapM token2ir tokens
-  Just $ (<>) "global _start\nsection .text\n" $ ir2asm ir
+  Just $ (<>) "global _start\nsection .text" $ ir2asm ir

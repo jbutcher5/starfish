bc5ba17bd834a065d0725c36dbbd276ee38ec020
James Butcher <jamesbutcher167@gmail.com>
2025-07-16 00:20:41 +0100

    Add a lower IR intermediate step

--- app/Compile.hs
+++ app/Compile.hs
@@ -3,18 +3,28 @@ module Compile where
 import Parse (Token (..))
 import GHC.Float (float2Int)
 import Data.Bits ((.&.))
+import Data.Maybe (fromJust)
 import Debug.Trace (trace)
+
+import qualified Data.HashMap.Strict as Map (HashMap, empty, insert, lookup)
+
+type Environment = Map.HashMap String Word
 
 data IR = FrameFunc String Word [IR] |
           LeafFunc String [IR] |
-          Variable String Word Word Int |
+          Variable String Word Word IR |
           Call String [IR] |
-          Inline String | Immediate Int
+          VarRef String |
+          Inline String | Immediate Int deriving (Show)
+
+data LowerIR = LoadVar String | Label String | Enter Word | Leave | Mov String String | AsmCall String | AsmInline String
+
+-- Phase 1: Convert to a more meaningful IR
 
 token2ir :: Token -> Maybe IR
 token2ir (Expr ((Ident "define"):xs)) =
   case xs of
-    [Ident label, Num x] -> Just . Variable label 4 0 $ float2Int x 
+    [Ident label, Num x] -> Just . Variable label 4 0 . Immediate $ float2Int x 
     _ -> Nothing
 token2ir (Expr ((Ident "defun"):(Ident name):xs)) = do
   ys <- mapM token2ir xs
@@ -24,37 +34,48 @@ token2ir (Expr ((Ident "defun"):(Ident name):xs)) = do
                   _ -> (x:acc, rbp)) ([], 0) ys :: ([IR], Word)
   Just $ FrameFunc name ((0xFFFFFFFFFFFFFFF0 .&. space) + 16) ir 
 token2ir (Expr [Ident "asm", Str asm]) = Just $ Inline asm
-token2ir (Num x) = Just . Immediate $ float2Int x 
+token2ir (Num x) = Just . Immediate $ float2Int x
+token2ir (Ident x) = Just $ VarRef x
 token2ir (Expr ((Ident fname):xs)) = do
   ys <- mapM token2ir xs
   Just $ Call fname ys
-token2ir x = trace (show x) Nothing
+token2ir x = trace ("token2ir error in token: " ++ show x) Nothing
 
-instr2asm :: IR -> String
-instr2asm (Immediate x) = "\nmov rax, " <> show x
-instr2asm (Variable ident bytes rbp x)
-  = "\nmov DWORD [rbp-" <> show rbp <> "], " <> show x
-instr2asm (FrameFunc name space body) 
-  = "\n" <> name <> ":\npush rbp\nmov rbp, rsp\nsub rsp, " <> show space <> ir2asm body <> "\nleave\nret"
-instr2asm (Inline asm) = "\n" <> asm
---instr2asm (Call fname args) = "\ncall " <> fname
+-- Phase 2: IR to Lower IR
 
--- TODO: Implement function calls 
-instr2asm (Call fname args) = buildParams args <> "\ncall " <> fname
-  where
+lowerIR :: IR -> [LowerIR]
+lowerIR (FrameFunc fname reserved body) =
+  [Label fname, Enter reserved] <> bodyir <> [Leave]
+  where bodyir = concatMap lowerIR body :: [LowerIR]
+lowerIR (Variable name size offset value) =
+  lowerIR value <> [Mov ("[rbp-" ++ show offset ++ "]") "rax"]
+lowerIR (Call fname args) = buildParams args <> [AsmCall fname]
+    where
     paramRegister = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"]
-    buildParams :: [IR] -> String
-    buildParams = setupParams . zip paramRegister 
+    buildParams :: [IR] -> [LowerIR]
+    buildParams = setupParams . zip paramRegister
 
-    setupParams :: [(String, IR)] -> String
-    setupParams = foldr (\(reg, ir) acc -> acc <> instr2asm ir <> "\nmov " <> reg <> ", rax") ""
+    setupParams :: [(String, IR)] -> [LowerIR] 
+    setupParams = foldr (\(reg, ir) acc -> acc <> lowerIR ir <> [Mov reg "rax"]) []
+lowerIR (Immediate v) = [Mov "rax" $ show v]
+lowerIR (VarRef ident) = [LoadVar ident]
+lowerIR (Inline asm) = [AsmInline asm] 
+lowerIR x = trace ("lowerIR error in token: " ++ show x) []
 
-instr2asm _ = ""
+-- Phase 3: Convert IR to Assembly
 
-ir2asm :: [IR] -> String
-ir2asm = foldr (\x acc -> instr2asm x <> acc) ""
+lowerIR2asm :: LowerIR -> String
+
+lowerIR2asm (LoadVar ident) = trace "fuck" ""
+lowerIR2asm (Label ident) = "\n" ++ ident ++ ":"
+lowerIR2asm (Enter reserved) = "\npush rbp\nmov rbp, rsp\nsub rsp, " ++ show reserved
+lowerIR2asm Leave = "\nleave"
+lowerIR2asm (Mov to from) = "\nmov " ++ to ++ ", " ++ from
+lowerIR2asm (AsmCall ident) = "\ncall " ++ ident
+lowerIR2asm (AsmInline asm) = "\n" ++ asm
 
 compile :: [Token] -> Maybe String
 compile tokens = do
   ir <- mapM token2ir tokens
-  Just $ (<>) "global _start\nsection .text" $ ir2asm ir
+  let asm = concatMap lowerIR2asm $ concatMap lowerIR ir
+  Just $ "global _start\nsection .text" <> asm
